55acddc42984c33f9b14eeb30ecd09ba45577842
El Wueno <dbueno@reyma.com.mx>
2025-08-21 16:56:56 -0600

    Fixed GetToolsStatus endpoint

--- Mobile.Remote.Toolkit.Business/Mobile.Remote.Toolkit.Business/Queries/Android/GetToolsStatusQuery.cs
+++ Mobile.Remote.Toolkit.Business/Mobile.Remote.Toolkit.Business/Queries/Android/GetToolsStatusQuery.cs
@@ -4,6 +4,7 @@ using MediatR;
 
 using Microsoft.Extensions.Logging;
 
+using Mobile.Remote.Toolkit.Business.Utils;
 using Mobile.Remote.Toolkit.Business.Queries.Base;
 using Mobile.Remote.Toolkit.Business.Services.Android;
 using Mobile.Remote.Toolkit.Business.Models.Responses.Android;
@@ -12,50 +13,38 @@ namespace Mobile.Remote.Toolkit.Business.Queries.Android
 {
     public sealed class GetToolsStatusQuery : BaseQuery<AndroidToolsStatusResponse>
     {
-
         public class GetToolsStatusQueryHandler : AndroidBaseQueryHandler<GetToolsStatusQuery, AndroidToolsStatusResponse>
         {
-            public GetToolsStatusQueryHandler(IAndroidDeviceService androidDeviceService, IMediator mediator, ILogger<GetToolsStatusQueryHandler> logger) : base(mediator, androidDeviceService) { }
+            private readonly IProcessHelper _processHelper;
+
+            public GetToolsStatusQueryHandler(IAndroidDeviceService androidDeviceService, IProcessHelper processHelper, IMediator mediator, ILogger<GetToolsStatusQueryHandler> logger) : base(mediator, androidDeviceService)
+            {
+                _processHelper = processHelper ?? throw new ArgumentNullException(nameof(processHelper));
+            }
 
             public async override Task<AndroidToolsStatusResponse> Handle(GetToolsStatusQuery request, CancellationToken cancellationToken)
             {
-                //try
-                //{
-                //    var adbResult = await Mediator.Send(new ExecuteAdbCommand
-                //    {
-                //        Serial = "",
-                //        Command = "version"
-                //    }, cancellationToken);
+                var response = new AndroidToolsStatusResponse();
+                try
+                {
+                    // Verificar ADB
+                    var adbResult = await _processHelper.ExecuteCommandAsync("adb", "version");
+                    response.AdbAvailable = adbResult.Success;
+                    response.AdbVersion = adbResult.Output?.Split('\n')[0];
 
-                //    var scrcpyResult = await Mediator.Send(new ExecuteScCommand
-                //    {
-                //        Serial = "",
-                //        Command = "version"
-                //    }, cancellationToken);
-
-                //    return new AndroidToolsStatusResponse
-                //    {
-                //        AdbAvailable = adbResult.Success,
-                //        AdbVersion = adbResult.Data?.GetValueOrDefault("output"),
-                //        ScrcpyAvailable = true,
-                //        ScrcpyVersion = true
-                //    });
-                //}
-                //catch (Exception ex)
-                //{
-                //return Ok(new
-                //{
-                //    success = false,
-                //    error = ex.Message,
-                //    tools = new
-                //    {
-                //        adb_available = false,
-                //        scrcpy_available = false
-                //    }
-                //});
-                //}
-                _ = await Mediator.Send(request, cancellationToken);
-                return new AndroidToolsStatusResponse { AdbAvailable = false };
+                    // Verificar scrcpy
+                    var scrcpyResult = await _processHelper.ExecuteCommandAsync("scrcpy", "--version");
+                    response.ScrcpyAvailable = scrcpyResult.Success;
+                    response.ScrcpyVersion = scrcpyResult.Output?.Split('\n')[0];
+                }
+                catch (Exception ex)
+                {
+                    response.AdbAvailable = false;
+                    response.ScrcpyAvailable = false;
+                    response.AdbVersion = $"Error: {ex.Message}";
+                    response.ScrcpyVersion = $"Error: {ex.Message}";
+                }
+                return response;
             }
         }
     }
